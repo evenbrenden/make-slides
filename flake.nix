cc77ee8682cd92c4eca88c76e17164fdb9acdbf2
Even Brenden <evenbrenden@users.noreply.github.com>
2024-01-24 19:24:14 +0100

    Tidy up flake.nix
    Remove stray direnv file.

--- flake.nix
+++ flake.nix
@@ -1,13 +1,11 @@
 {
   description = "make-slides";
 
+  # NIXPKGS_ALLOW_UNFREE=1 nix run --impure
+
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      packages.x86_64-linux.make-slides = import ./make-slides.nix { inherit pkgs; };
-      packages.x86_64-linux.default = self.packages.x86_64-linux.make-slides;
-
-    };
+    in { packages.x86_64-linux.default = import ./make-slides.nix { inherit pkgs; }; };
 }
