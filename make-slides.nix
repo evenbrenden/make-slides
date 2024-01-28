7472a9c2cd2fb256f1e5c87b278d54bf789179a6
Even Brenden <evenbrenden@users.noreply.github.com>
2024-01-28 14:46:19 +0100

    Refactor

--- make-slides.nix
+++ make-slides.nix
@@ -4,27 +4,29 @@ with pkgs;
 
 let
   # https://github.com/jgm/pandoc/pull/9204
-  fonts = rec {
-    main-directory = "${pkgs.freefont_ttf}/share/fonts/truetype";
-    main-file = "FreeSerif.ttf";
-    bold = "${main-directory}/FreeSerifBold.ttf";
-    italic = "${main-directory}/FreeSerifItalic.ttf";
-    bold-italic = "${main-directory}/FreeSerifBoldItalic.ttf";
-  };
+  font-directory = "${pkgs.freefont_ttf}/share/fonts/truetype";
+  metadata-file = writeText "metadata.yaml" ''
+    ---
+    colorlinks: true
+    mainfont: FreeSerif
+    mainfontoptions:
+    - BoldFont=FreeSerifBold
+    - ItalicFont=FreeSerifItalic
+    - BoldItalicFont=FreeSerifBoldItalic
+    ---
+  '';
 in writeShellApplication {
   name = "make-slides";
   runtimeInputs = [ pandoc texlive.combined.scheme-medium ];
   text = ''
     # https://tex.stackexchange.com/a/313605
     SOURCE_DATE_EPOCH=0 \
-    OSFONTDIR=${fonts.main-directory} \
+    OSFONTDIR=${font-directory} \
     pandoc \
       --from=markdown \
       --to=beamer \
       --pdf-engine=lualatex \
-      --variable='colorlinks:true' \
-      --variable='mainfont:${fonts.main-file}' \
-      --variable='mainfontoptions:BoldFont=${fonts.bold}, ItalicFont=${fonts.italic}, BoldItalicFont=${fonts.bold-italic}' \
+      --metadata-file=${metadata-file} \
       --output=slides.pdf \
       slides.md
   '';
